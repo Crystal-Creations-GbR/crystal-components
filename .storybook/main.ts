f5bf59cbb5952469ea0f56ed6e73563531a8ea08
Johannes Huther <dev.johannes@huther.link>
2024-01-27 22:10:35 +0100

    test: add storybook and deployment to chromatic

---
+++ .storybook/main.ts
@@ -0,0 +1,25 @@
+import type { StorybookConfig } from "@storybook/vue3-vite";
+
+const config: StorybookConfig = {
+  stories: [
+    "../stories/**/*.mdx",
+    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/vue3-vite",
+    options: {},
+  },
+  core: {
+    disableTelemetry: true,
+  },
+  docs: {
+    autodocs: true,
+  },
+};
+
+export default config;
---
+++ vuetify-options.ts
@@ -0,0 +1,35 @@
+import { VuetifyOptions } from "vuetify";
+import { aliases, mdi } from "vuetify/iconsets/mdi-svg";
+
+const options: VuetifyOptions = {
+  theme: {
+    defaultTheme: "light",
+    themes: {
+      light: {
+        dark: false,
+        colors: {
+          primary: "#de735f",
+          secondary: "#3d4059",
+          accent: "#eeb06e",
+          error: "#f44336",
+          info: "#2196F3",
+          success: "#4CAF50",
+          warning: "#FB8C00",
+          background: "#ededed",
+          surface: "#FFFFFF",
+        },
+        variables: {},
+      },
+    },
+  },
+  icons: {
+    defaultSet: "mdi",
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+  ssr: true,
+};
+
+export default options;
