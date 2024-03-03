7df24e7f0ab9aa1cecd4e585a41f00568b7657c0
Pierre Fischer <pierre@fischer-ab.de>
2024-03-03 23:11:29 +0100

    feat: add component `CSnackbar`

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import CLicenseCard from "./components/CLicenseCard.vue";
 import CItemListGroup from "./components/CItemListGroup.vue";
+import CSnackbar from "./components/CSnackbar.vue";
 export { AnimatedCheckmark } from "@crystal-creations/animated-checkmark";
 export { PulsatingCircle } from "@crystal-creations/pulsating-circle";
 
 export { CLicenseCard };
 export { CItemListGroup };
+export { CSnackbar };
