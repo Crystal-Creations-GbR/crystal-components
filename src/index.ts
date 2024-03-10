53b18c9b923024a15a73a3d73f551ab78be49700
Pierre Fischer <pierre@fischer-ab.de>
2024-03-10 16:28:39 +0100

    feat: add component `CNavigationDrawer`

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import CLicenseCard from "./components/CLicenseCard.vue";
 import CItemListGroup from "./components/CItemListGroup.vue";
 import CSnackbar from "./components/CSnackbar.vue";
 import CSnackbarManager from "./components/CSnackbarManager.vue";
+import CNavigationDrawer from "./components/CNavigationDrawer.vue";
 export { AnimatedCheckmark } from "@crystal-creations/animated-checkmark";
 export { PulsatingCircle } from "@crystal-creations/pulsating-circle";
 
@@ -9,3 +10,4 @@ export { CLicenseCard };
 export { CItemListGroup };
 export { CSnackbar };
 export { CSnackbarManager };
+export { CNavigationDrawer };
