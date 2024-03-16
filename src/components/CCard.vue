16d9c0e73f593c857ac283057b5a98ff37ad6ea4
Pierre Fischer <pierre@fischer-ab.de>
2024-03-16 23:58:02 +0100

    fix: fix scrolling inside a `CCardDialog`

--- src/components/CCard.vue
+++ src/components/CCard.vue
@@ -6,13 +6,7 @@
     :width="width"
     :max-width="maxWidth"
     class="c-card pt-6 pt-md-8"
-    :class="
-      slots['content-full-width'] ||
-      slots['actions'] ||
-      slots['prepend-actions']
-        ? ''
-        : 'pb-6 pb-md-8'
-    "
+    :class="cardClass"
     :loading="loading"
   >
     <template #loader="{ isActive }">
@@ -24,15 +18,20 @@
       ></v-progress-linear>
     </template>
 
-    <div class="c-card-sections">
-      <v-card-title
-        v-if="title"
-        class="pt-0 pb-0 px-6 px-md-8 text-h6"
-        :class="titleClass"
-      >
-        {{ title }}
-      </v-card-title>
+    <v-card-title
+      v-if="title"
+      class="pt-0 pb-0 mb-3 px-6 px-md-8 text-h6"
+      :class="titleClass"
+    >
+      {{ title }}
+    </v-card-title>
 
+    <v-divider v-if="scrollable && title" class="mb-0"></v-divider>
+
+    <v-card-text
+      ref="cardTextComponent"
+      :class="scrollable ? 'ma-0 px-0' : 'ma-0 pa-0'"
+    >
       <v-card-subtitle
         v-if="subtitle"
         class="c-card-subtitle px-6 px-md-8 text-wrap"
@@ -49,21 +48,27 @@
       <div v-if="slots['content-full-width']">
         <slot name="content-full-width" :padding="'px-6 px-md-8'"></slot>
       </div>
+    </v-card-text>
 
-      <v-card-actions
-        v-if="slots['actions'] || slots['prepend-actions']"
-        class="mt-2 mb-3 px-3 px-md-6 d-flex"
-      >
-        <slot name="prepend-actions"></slot>
-        <v-spacer></v-spacer>
-        <slot name="actions"></slot>
-      </v-card-actions>
-    </div>
+    <v-divider
+      v-if="scrollable && (slots['actions'] || slots['prepend-actions'])"
+      class="ma-0"
+    ></v-divider>
+
+    <v-card-actions
+      v-if="slots['actions'] || slots['prepend-actions']"
+      class="mt-2 mb-3 px-3 px-md-6 d-flex"
+    >
+      <slot name="prepend-actions"></slot>
+      <v-spacer></v-spacer>
+      <slot name="actions"></slot>
+    </v-card-actions>
   </v-card>
 </template>
 
 <script setup lang="ts">
-import { computed, useSlots } from "vue";
+import { computed, onMounted, ref, useSlots } from "vue";
+import { VCardText } from "vuetify/components";
 
 const slots = useSlots();
 
@@ -121,6 +126,24 @@ const props = withDefaults(
 );
 
 /**
+ * The reference to the `v-card-text` component.
+ */
+const cardTextComponent = ref<VCardText>();
+
+const cardClass = computed<string>(() => {
+  if (
+    scrollable.value ||
+    slots["content-full-width"] ||
+    slots.actions ||
+    slots["prepend-actions"]
+  ) {
+    return "";
+  }
+
+  return "pb-6 pb-md-8";
+});
+
+/**
  * Computes the classes for the title component.
  */
 const titleClass = computed<string>(() => {
@@ -129,7 +152,27 @@ const titleClass = computed<string>(() => {
   if (props.titleSize && props.titleSize !== "normal")
     classes += " " + props.titleSize;
 
+  if (scrollable.value) classes += " mb-5 mb-md-7";
+
   return classes;
+});
+
+/**
+ * Whether the content inside the `v-card-text` component is large enough to scroll.
+ */
+const scrollable = ref<boolean>(false);
+
+/**
+ * Checks, if the content inside the `v-card-text` is scrollable.
+ */
+onMounted(() => {
+  if (
+    cardTextComponent.value &&
+    cardTextComponent.value.$el.scrollHeight >
+      cardTextComponent.value.$el.clientHeight
+  ) {
+    scrollable.value = true;
+  }
 });
 </script>
 
@@ -137,9 +180,9 @@ const titleClass = computed<string>(() => {
 @import "vuetify/lib/styles/settings/variables";
 
 .c-card {
-  .c-card-sections > * {
+  & > .v-card-text > * {
     &:not(:last-child) {
-      margin-bottom: 12px;
+      margin-bottom: 12px !important;
     }
   }
 
